d2900cbb5d5cce7f2cd68bea35d108d623df888b
Arun Singh <arunsinghsnd@gmail.com>
2026-01-26 09:10:05 +0530

    feat: create auth-store

---
+++ src/store/auth-store.js
@@ -0,0 +1,47 @@
+import { create } from "zustand";
+import { apiFetch } from "@/lib/api";
+
+export const useAuthStore = create((set) => ({
+  user: null,
+  loading: true,
+
+  fetchUser: async () => {
+    try {
+      const res = await apiFetch(
+        "/api/v1/auth/current-user"
+      );
+
+      if (!res.ok) throw new Error();
+
+      const data = await res.json();
+      set({ user: data.data, loading: false });
+    } catch {
+      set({ user: null, loading: false });
+    }
+  },
+
+  login: async (payload) => {
+    const res = await apiFetch(
+      "/api/v1/auth/login",
+      {
+        method: "POST",
+        body: JSON.stringify(payload),
+      }
+    );
+
+    if (!res.ok) {
+      const err = await res.json();
+      throw err;
+    }
+
+    await apiFetch("/api/v1/auth/current-user");
+  },
+
+  logout: async () => {
+    await apiFetch("/api/v1/auth/logout", {
+      method: "POST",
+    });
+
+    set({ user: null });
+  },
+}));
