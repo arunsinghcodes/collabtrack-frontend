78a596b10d3f755f4b69e4bafc9362d6420b58aa
Arun Singh <arunsinghsnd@gmail.com>
2026-01-28 23:44:16 +0530

    added the banner

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/store/auth-store";
 import Sidebar from "@/components/layout/sidebar";
 import Navbar from "@/components/layout/navbar";
+import EmailVerificationBanner from "@/components/auth/email-verification-banner";
 
 export default function DashboardLayout({ children }) {
   const router = useRouter();
@@ -37,6 +38,7 @@ export default function DashboardLayout({ children }) {
       <Sidebar />
 
       <div className="flex flex-col flex-1">
+        <EmailVerificationBanner user={user} />
         <Navbar />
         <main className="flex-1 overflow-y-auto p-6 bg-background">
           {children}
