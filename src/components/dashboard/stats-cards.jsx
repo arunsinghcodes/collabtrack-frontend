02059b7a04dd4fe3cde81f89505e0e1646fcdd98
Arun Singh <arunsinghsnd@gmail.com>
2026-02-20 01:01:21 +0530

    Add: getProjects api

--- src/components/dashboard/stats-cards.jsx
+++ src/components/dashboard/stats-cards.jsx
@@ -1,16 +1,33 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle
-} from "@/components/ui/card";
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useEffect, useState } from "react";
 
 export default function StatsCards() {
   const stats = [
     { title: "Projects", value: 4 },
     { title: "My Tasks", value: 12 },
-    { title: "Due Soon", value: 3 }
+    { title: "Due Soon", value: 3 },
   ];
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const getProjects = async () => {
+      const response = await fetch(
+        "https://collabtrack-api.onrender.com/api/v1/projects",
+        {
+          credentials: "include",
+        },
+      );
+      const result = await response.json();
+      setProjects(result?.data);
+    };
+
+    getProjects();
+  }, []);
+
+  console.log(projects);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
@@ -20,7 +37,7 @@ export default function StatsCards() {
             <CardTitle>{stat.title}</CardTitle>
           </CardHeader>
           <CardContent>
-            <p className="text-3xl font-bold">{stat.value}</p>
+            <p className="text-3xl font-bold">{projects.length}</p>
           </CardContent>
         </Card>
       ))}
